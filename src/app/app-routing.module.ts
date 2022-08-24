2aa22360f45b34a835fe500574edf2fe400a5272
zydplatform <kigandaivan32@gmail.com>
2022-08-24 13:37:31 +0300

    update-ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ import { RegisterComponent } from './views/pages/register/register.component';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
