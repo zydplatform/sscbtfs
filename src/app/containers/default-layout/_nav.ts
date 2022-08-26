f56965e6c4c84cb8449cc390fe4d2feb7a506098
zydplatform <kigandaivan32@gmail.com>
2022-08-26 08:59:58 +0300

    update-ui

--- src/app/containers/default-layout/_nav.ts
+++ src/app/containers/default-layout/_nav.ts
@@ -16,12 +16,12 @@ export const navItems: INavData[] = [
   },
   {
     name: 'System Users ',
-    url: '/theme/colors',
+    // url: '/theme/colors',
     iconComponent: { name: 'cil-drop' }
   },
   {
     name: 'Add System User',
-    url: '/theme/typography',
+    // url: '/theme/typography',
     linkProps: { fragment: 'someAnchor' },
     iconComponent: { name: 'cil-pencil' }
   },
@@ -31,24 +31,24 @@ export const navItems: INavData[] = [
   },
   {
     name: 'Border Points',
-    url: '/base',
+    // url: '/base',
     iconComponent: { name: 'cil-puzzle' },
     children: [
       {
         name: 'Bunagana',
-        url: '/base/accordion'
+        // url: '/base/accordion'
       },
       {
         name: 'Mpondwe',
-        url: '/base/breadcrumbs'
+        // url: '/base/breadcrumbs'
       },
       {
         name: 'Katuna',
-        url: '/base/cards'
+        // url: '/base/cards'
       },
       {
         name: 'Malaba',
-        url: '/base/carousel'
+        // url: '/base/carousel'
       },
       // {
       //   name: 'Collapse',
@@ -98,43 +98,43 @@ export const navItems: INavData[] = [
   },
   {
     name: 'Agencies',
-    url: '/buttons',
+    // url: '/buttons',
     iconComponent: { name: 'cil-cursor' },
     children: [
       {
         name: 'Immigration',
-        url: '/buttons/buttons'
+        // url: '/buttons/buttons'
       },
       {
         name: 'URA',
-        url: '/buttons/button-groups'
+        // url: '/buttons/button-groups'
       },
       {
         name: 'Ministry of Trade',
-        url: '/buttons/dropdowns'
+        // url: '/buttons/dropdowns'
       },
     ]
   },
   {
     name: 'Goods',
-    url: '/forms',
+    // url: '/forms',
     iconComponent: { name: 'cil-notes' },
     children: [
       {
         name: 'Trader & Goods',
-        url: '/forms/form-control'
+        // url: '/forms/form-control'
       },
       {
         name: 'GoodsRegistered',
-        url: '/forms/select'
+        // url: '/forms/select'
       },
       {
         name: 'GoodsDeclared',
-        url: '/forms/checks-radios'
+        // url: '/forms/checks-radios'
       },
       {
         name: 'GoodsNotDeclared',
-        url: '/forms/range'
+        // url: '/forms/range'
       },
       // {
       //   name: 'Input Group',
@@ -156,17 +156,17 @@ export const navItems: INavData[] = [
   },
   {
     name: 'BorderStatisticalData',
-    url: '/charts',
+    // url: '/charts',
     iconComponent: { name: 'cil-chart-pie' }
   },
   {
     name: 'BorderTransactions',
     iconComponent: { name: 'cil-star' },
-    url: '/icons',
+    // url: '/icons',
     children: [
       {
         name: 'Exports',
-        url: '/icons/coreui-icons',
+        // url: '/icons/coreui-icons',
         // badge: {
         //   color: 'success'
           
@@ -174,11 +174,11 @@ export const navItems: INavData[] = [
       },
       {
         name: 'Imports',
-        url: '/icons/flags'
+        // url: '/icons/flags'
       },
       {
         name: 'CustomsClearance',
-        url: '/icons/brands'
+        // url: '/icons/brands'
       }
     ]
   },
@@ -188,15 +188,15 @@ export const navItems: INavData[] = [
     children: [
       {
         name: 'All Traders',
-        url: '/notifications/alerts'
+        // url: '/notifications/alerts'
       },
       {
         name: 'TraderDeclarations',
-        url: '/notifications/alerts'
+        // url: '/notifications/alerts'
       },
       {
         name: 'TraderFacilitation',
-        url: '/notifications/badges'
+        // url: '/notifications/badges'
       },
       // {
       //   name: '',
@@ -221,27 +221,27 @@ export const navItems: INavData[] = [
   //   title: true,
   //   name: 'Extras'
   // },
-  {
-    name: 'Pages',
-    url: '/login',
-    iconComponent: { name: 'cil-star' },
-    children: [
-      {
-        name: 'Login',
-        url: '/login'
-      },
-      {
-        name: 'Register',
-        url: '/register'
-      },
-      {
-        name: 'Error 404',
-        url: '/404'
-      },
-      {
-        name: 'Error 500',
-        url: '/500'
-      }
-    ]
-  },
+  // {
+  //   name: 'Pages',
+  //   url: '/login',
+  //   iconComponent: { name: 'cil-star' },
+  //   children: [
+  //     {
+  //       name: 'Login',
+  //       url: '/login'
+  //     },
+  //     {
+  //       name: 'Register',
+  //       url: '/register'
+  //     },
+  //     {
+  //       name: 'Error 404',
+  //       url: '/404'
+  //     },
+  //     {
+  //       name: 'Error 500',
+  //       url: '/500'
+  //     }
+  //   ]
+  // },
 ];
